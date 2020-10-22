d9a4aeb3032693c560fcb57c91b5809f11012f27
lingfeng <413957294@qq.com>
2020-10-22 14:53:48 +0800

    完善代码

--- gateway-demo/src/main/java/com/bulingfeng/GatewayApplication.java
+++ gateway-demo/src/main/java/com/bulingfeng/GatewayApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 参考文档
+ * https://blog.csdn.net/xiaolaoban212/article/details/100654334
+ * https://developer.aliyun.com/article/640801
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
@@ -11,5 +16,4 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
--- servicec-lient/src/main/java/com/lingeng/controller/ClientController.java
+++ servicec-lient/src/main/java/com/lingeng/controller/ClientController.java
@@ -1,8 +1,11 @@
 package com.lingeng.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author:bulingfeng
@@ -11,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/client")
 public class ClientController {
-
+    AtomicInteger ac = new AtomicInteger();
     @GetMapping("/test")
-    public String requestClientTest(){
+    public String requestClientTest(String name){
+        System.out.println(ac.getAndAdd(1));
         return "request service-client-1 is successful";
     }
 }
