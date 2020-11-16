ca530be6030ef720524db11a419437ffd9797565
lingfeng <413957294@qq.com>
2020-11-16 15:12:34 +0800

    日志的配置

---
+++ log-service/src/main/java/com/bulingfeng/LogApplication.java
@@ -0,0 +1,15 @@
+package com.bulingfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:bulingfeng
+ * @Date: 2020-11-16
+ */
+@SpringBootApplication
+public class LogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class,args);
+    }
+}
