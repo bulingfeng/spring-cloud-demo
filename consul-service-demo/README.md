81e5d3460e68b1ea63b1a7b931e05db142003361
lingfeng <413957294@qq.com>
2020-12-21 11:38:39 +0800

    consul服务的demo

---
+++ consul-service-demo/README.md
@@ -0,0 +1,10 @@
+## consul的登录地址
+```aidl
+http://127.0.0.1:8500/ui/dc1/kv
+consul.io/docs/dynamic-app-config/sessions
+```
+
+## 参考文档
+```
+https://www.cnblogs.com/mrhelloworld/p/consul-config.html
+```
---
+++ consul-service-demo/src/main/java/com/bulingfeng/consul/ConsulProducerApplication.java
@@ -0,0 +1,17 @@
+package com.bulingfeng.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @Author:bulingfeng
+ * @Date: 2020/12/18
+ */
+@SpringBootApplication
+public class ConsulProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulProducerApplication.class,args);
+    }
+}
